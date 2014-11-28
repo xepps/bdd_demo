12ca97150b6fbd0764e856f65677737722927714
Kristian Epps <kristian.epps@bbc.co.uk>
2014-11-28 14:48:38 +0000

    Added ability to add 3 numbers

--- features/Addition.feature
+++ features/Addition.feature
@@ -8,3 +8,8 @@ Feature: Addition
 		And I have entered 70 into the calculator
 		When I add them together
 		Then the result should be 120
+
+	Scenario: Add three numbers
+		Given I have already added 50 and 70
+		When I add 30
+		Then the result should be 150
--- features/step_definitions/addition-steps.rb
+++ features/step_definitions/addition-steps.rb
@@ -6,7 +6,18 @@ When(/^I add them together$/) do
 	@total = @calculator.total
 end
 
-Then(/^the result should be (\d+)$/) do |arg1|
-  expect(@total).to eq(120)
+Then(/^the result should be (\d+)$/) do |total|
+  expect(@total).to eq(total.to_i)
+end
+
+Given(/^I have already added (\d+) and (\d+)$/) do |number_1, number_2|
+  @calculator = push_number_to_calculator(@calculator, number_1.to_i)
+  @calculator = push_number_to_calculator(@calculator, number_2.to_i)
+  @calculator.total
+end
+
+When(/^I add (\d+)$/) do |number|
+  @calculator = push_number_to_calculator(@calculator, number.to_i)
+  @total = @calculator.total
 end
 
